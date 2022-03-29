6b3151f67ebda41d698480a4ecb19875d434c874
ebrahemsamer2 <ebrahemsamer2@gmail.com>
2022-03-29 22:51:01 +0200

    Working on Product & Category Models and Migrations

---
+++ app/Models/Category.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Models\Product;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
